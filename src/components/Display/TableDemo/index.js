bdabc80b29fcd2c77f1a27d4c5a76b85c240301e
zhangzhihao <137596665@qq.com>
2018-06-22 21:45:42 +0800

    完善TableDemo

--- src/components/Display/TableDemo/index.js
+++ src/components/Display/TableDemo/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Tooltip, Button, Icon, Table, Divider, BackTop, Affix, Anchor } from 'antd'
+import { Card, Popconfirm , Button, Icon, Table, Divider, BackTop, Affix, Anchor,Form,InputNumber,Input } from 'antd'
 import axios from 'axios'
 import CustomBreadcrumb from '../../../common/CustomBreadcrumb/index'
 
@@ -157,21 +157,174 @@ for (let i = 0; i < 100; i++) {
     address: `London Park no. ${i}`,
   });
 }
-
+const data8 = [];
+for (let i = 0; i < 100; i++) {
+  data8.push({
+    key: i.toString(),
+    name: `Edrward ${i}`,
+    age: 32,
+    address: `London Park no. ${i}`,
+  });
+}
+const FormItem = Form.Item;
+const EditableContext = React.createContext();
+const EditableRow = ({ form, index, ...props }) => (
+  <EditableContext.Provider value={form}>
+    <tr {...props} />
+  </EditableContext.Provider>
+);
+const EditableFormRow = Form.create()(EditableRow);
+class EditableCell extends React.Component {
+  getInput = () => {
+    if (this.props.inputType === 'number') {
+      return <InputNumber />;
+    }
+    return <Input />;
+  };
+  render() {
+    const {
+      editing,
+      dataIndex,
+      title,
+      inputType,
+      record,
+      index,
+      ...restProps
+    } = this.props;
+    return (
+      <EditableContext.Consumer>
+        {(form) => {
+          const { getFieldDecorator } = form;
+          return (
+            <td {...restProps}>
+              {editing ? (
+                <FormItem style={{ margin: 0 }}>
+                  {getFieldDecorator(dataIndex, {
+                    rules: [{
+                      required: true,
+                      message: `Please Input ${title}!`,
+                    }],
+                    initialValue: record[dataIndex],
+                  })(this.getInput())}
+                </FormItem>
+              ) : restProps.children}
+            </td>
+          );
+        }}
+      </EditableContext.Consumer>
+    );
+  }
+}
 class TableDemo extends React.Component {
   state = {
     filteredInfo: null,
     sortedInfo: null,
     loading:false,
     data4:[],
-    pagination:{}
+    pagination:{
+      pageSize:8
+    },
+    data7:[{
+      key: '0',
+      name: 'Edward King 0',
+      age: '32',
+      address: 'London, Park Lane no. 0',
+    },{
+      key: '1',
+      name: 'Edward King 1',
+      age: '32',
+      address: 'London, Park Lane no. 1',
+    }],
+    count: 2,
+    data8,
+    editingKey: '',
   }
   componentDidMount(){
     this.getRemoteData()
   }
+  columns7 = [
+    {
+      title: 'name',
+      dataIndex: 'name',
+      width: '30%',
+    },
+    {
+      title: 'age',
+      dataIndex: 'age',
+    },
+    {
+      title: 'address',
+      dataIndex: 'address',
+    },
+    {
+      title: 'operation',
+      dataIndex: 'operation',
+      render:(text, record)=>{
+        return (
+          this.state.data7.length>1?
+          <Popconfirm title="Sure to delete?" onConfirm={() => this.onDelete(record.key)}>
+            <a href="javascript:;">Delete</a>
+          </Popconfirm> : null
+        )
+      }
+    }
+  ]
+  columns8 = [
+    {
+      title: 'name',
+      dataIndex: 'name',
+      width: '25%',
+      editable: true,
+    },
+    {
+      title: 'age',
+      dataIndex: 'age',
+      width: '15%',
+      editable: true,
+    },
+    {
+      title: 'address',
+      dataIndex: 'address',
+      width: '40%',
+      editable: true,
+    },
+    {
+      title: 'operation',
+      dataIndex: 'operation',
+      render: (text, record) => {
+        const editable = this.isEditing(record);
+        return (
+          <div>
+            {editable ? (
+              <span>
+                  <EditableContext.Consumer>
+                    {form => (
+                      <a
+                        href="javascript:;"
+                        onClick={() => this.save(form, record.key)}
+                        style={{ marginRight: 8 }}
+                      >
+                        Save
+                      </a>
+                    )}
+                  </EditableContext.Consumer>
+                  <Popconfirm
+                    title="Sure to cancel?"
+                    onConfirm={() => this.cancel(record.key)}
+                  >
+                    <a>Cancel</a>
+                  </Popconfirm>
+                </span>
+            ) : (
+              <a onClick={() => this.edit(record.key)}>Edit</a>
+            )}
+          </div>
+        );
+      },
+    },
+  ]
 
   handleChange = (pagination, filters, sorter) => {
-    console.log('Various parameters', pagination, filters, sorter)
     this.setState({
       filteredInfo: filters,
       sortedInfo: sorter,
@@ -228,6 +381,54 @@ class TableDemo extends React.Component {
       ...filters,
     })
   }
+  onDelete=(key)=>{
+    const arr = this.state.data7.slice()
+    this.setState({
+      data7:arr.filter(item=>item.key!==key)
+    })
+  }
+  handleAdd=()=>{
+    const {data7,count} = this.state //本来想用data7的length来代替count，但是删除行后，length会-1
+    const newData = {
+      key: count,
+      name: `Edward King ${count}`,
+      age: 32,
+      address: `London, Park Lane no. ${count}`,
+    };
+    this.setState({
+      data7:[...data7,newData],
+      count:count+1
+    })
+  }
+  isEditing = (record) => {
+    return record.key === this.state.editingKey;
+  };
+  edit(key) {
+    this.setState({ editingKey: key });
+  }
+  save(form, key) {
+    form.validateFields((error, row) => {
+      if (error) {
+        return;
+      }
+      const newData = [...this.state.data8];
+      const index = newData.findIndex(item => key === item.key);
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, {
+          ...item,
+          ...row,
+        });
+        this.setState({ data8: newData, editingKey: '' });
+      } else {
+        newData.push(data8);
+        this.setState({ data8: newData, editingKey: '' });
+      }
+    });
+  }
+  cancel = () => {
+    this.setState({ editingKey: '' });
+  };
 
   render () {
     const rowSelection = {
@@ -268,6 +469,27 @@ class TableDemo extends React.Component {
         sorter: (a, b) => a.address.length - b.address.length,
         sortOrder: sortedInfo.columnKey === 'address' && sortedInfo.order,
       }]
+    const components = {
+      body: {
+        row: EditableFormRow,
+        cell: EditableCell,
+      },
+    };
+    const columns8 = this.columns8.map((col) => {
+      if (!col.editable) {
+        return col;
+      }
+      return {
+        ...col,
+        onCell: record => ({
+          record,
+          inputType: col.dataIndex === 'age' ? 'number' : 'text',
+          dataIndex: col.dataIndex,
+          title: col.title,
+          editing: this.isEditing(record),
+        }),
+      };
+    });
     return (
       <div>
         <CustomBreadcrumb arr={['显示', '表格']}/>
@@ -309,6 +531,13 @@ class TableDemo extends React.Component {
           <Table dataSource={data6} columns={columns6} style={styles.tableStyle}
                  scroll={{ x: 1500, y: 500 }}/>
         </Card>
+        <Card bordered={false} title='可编辑的表格' style={{marginBottom: 15,minHeight:440}} id='editTable'>
+          <p>
+            <Button onClick={this.handleAdd}>添加行</Button>
+          </p>
+          <Table bordered dataSource={this.state.data7} columns={this.columns7} style={styles.tableStyle}/>
+          <Table style={styles.tableStyle} components={components} bordered dataSource={this.state.data8} columns={columns8}/>
+        </Card>
         <BackTop visibilityHeight={200} style={{right: 50}}/>
         <Affix style={styles.affixBox}>
           <Anchor offsetTop={50} affix={false}>
@@ -319,6 +548,7 @@ class TableDemo extends React.Component {
             <Anchor.Link href='#remoteLoading' title='远程加载数据'/>
             <Anchor.Link href='#unfold' title='可展开'/>
             <Anchor.Link href='#fixed' title='固定头和列'/>
+            <Anchor.Link href='#editTable' title='可编辑的表格'/>
           </Anchor>
         </Affix>
       </div>
