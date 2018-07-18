3b9dba81f51664b7d9076662390cfa367693ae31
zhangzhihao <137596665@qq.com>
2018-07-18 21:27:03 +0800

    重新设计了登录页面的效果

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -1,22 +1,68 @@
 import React from 'react'
 import BGParticle from '../../utils/BGParticle'
-import {Form,Input} from 'antd'
+import {Form,Input,Row,Col} from 'antd'
 import './style.css'
+import {randomNum} from '../../utils/utils'
 
 @Form.create()
 class LoginForm extends React.Component{
   state = {
-    focusItem:-1   //保存当前聚焦的input
+    focusItem:-1,   //保存当前聚焦的input
+    code:''         //验证码
+  }
+  componentDidMount(){
+    this.createCode()
+  }
+
+  /**
+   * 生成验证码
+   */
+  createCode = ()=>{
+    const ctx = this.canvas.getContext('2d')
+    const chars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    let code = ''
+    ctx.clearRect(0, 0, 80, 39);
+    for(let i=0;i<4;i++){
+      const char = chars[randomNum(0,57)]
+      code += char
+      ctx.font = randomNum(20, 25) + 'px SimHei'  //设置字体随机大小
+      ctx.fillStyle = '#D3D7F7';
+      ctx.textBaseline = 'middle';
+      ctx.shadowOffsetX = randomNum(-3, 3);
+      ctx.shadowOffsetY = randomNum(-3, 3);
+      ctx.shadowBlur = randomNum(-3, 3);
+      ctx.shadowColor = "rgba(0, 0, 0, 0.3)";
+      let x = 80 / 5 * (i + 1);
+      let y = 39 / 2;
+      let deg = randomNum(-25, 25);
+      /**设置旋转角度和坐标原点**/
+      ctx.translate(x, y);
+      ctx.rotate(deg * Math.PI / 180);
+      ctx.fillText(char, 0, 0);
+      /**恢复旋转角度和坐标原点**/
+      ctx.rotate(-deg * Math.PI / 180);
+      ctx.translate(-x, -y);
+    }
+    this.setState({
+      code
+    })
+  }
+  loginSubmit = (e)=>{
+    e.preventDefault()
+    console.log(e)
   }
   render(){
-    const {getFieldDecorator} = this.props.form
+    const {getFieldDecorator,getFieldError} = this.props.form
     const {focusItem} = this.state
+    console.log(getFieldError('username'))
     return (
       <div className={this.props.className}>
         <h3 className='title'>管理员登录</h3>
-        <Form>
-          <Form.Item>
-            {getFieldDecorator('username')(
+        <Form onSubmit={this.loginSubmit}>
+          <Form.Item help={getFieldError('username') && <div>fdsafafdasdfsaf</div>}>
+            {getFieldDecorator('username',{
+              rules: [{required: true, message: '请输入用户名'}]
+            })(
               <Input
                 onFocus={() => this.setState({focusItem: 0})}
                 onBlur={() => this.setState({focusItem: -1})}
@@ -37,18 +83,31 @@ class LoginForm extends React.Component{
             )}
           </Form.Item>
           <Form.Item>
-            {getFieldDecorator('password2')(
-              <Input
-                onFocus={() => this.setState({focusItem: 2})}
-                onBlur={() => this.setState({focusItem: -1})}
-                type='password'
-                maxLength={16}
-                placeholder='密码'
-                addonBefore={<span className='iconfont icon-suo1' style={focusItem === 2 ? styles.focus : {}}/>}/>
+            {getFieldDecorator('verification')(
+              <Row>
+                <Col span={15}>
+                  <Input
+                    onFocus={() => this.setState({focusItem: 2})}
+                    onBlur={() => this.setState({focusItem: -1})}
+                    type='password'
+                    maxLength={16}
+                    placeholder='验证码'
+                    addonBefore={<span className='iconfont icon-securityCode-b' style={focusItem === 2 ? styles.focus : {}}/>}/>
+                </Col>
+                <Col span={9}>
+                  <canvas onClick={this.createCode} width="80" height='39' ref={el=>this.canvas=el}/>
+                </Col>
+              </Row>
             )}
           </Form.Item>
+          <div>
+            <input className='loginBtn' type="submit" value='登录'/>
+          </div>
         </Form>
-        <button onClick={()=>this.props.switchShowBox('register')}>click</button>
+        <div className='footer'>
+          <div>欢迎登陆后台管理系统</div>
+        </div>
+        {/*<button onClick={()=>this.props.switchShowBox('register')}>click</button>*/}
       </div>
     )
   }
@@ -113,7 +172,8 @@ const styles = {
   },
   focus:{
     // transform: 'scale(0.7)',
-    width:'20px'
+    width:'20px',
+    opacity:1
   }
 }
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -32,3 +32,13 @@ export function digitUppercase(n) {
     .replace(/(零.)+/g, '零')
     .replace(/^整$/, '零元整');
 }
+
+/**
+ * 生成指定区间的随机整数
+ * @param min
+ * @param max
+ * @returns {number}
+ */
+export function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
