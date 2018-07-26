36da87fbaeed31ce172c7a502807119b91f3a3d8
zhangzhihao <137596665@qq.com>
2018-07-26 21:20:05 +0800

    home页面用背景图片属性代替img标签，解决图片大小出现问题

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -6,11 +6,24 @@ class Home extends React.Component {
   render() {
     return (
       <div style={styles.bg} className='home'>
-        <Carousel arrows effect='fade' style={styles.size}>
-          <img style={styles.size} src="https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-343369.jpg" alt="" />
-          <img style={styles.size} src="https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-4871.jpg" alt="" />
-          <img style={styles.size} src="https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-486302.jpg" alt="" />
-          <img style={styles.size} src="https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-75202.jpg" alt="" />
+        <Carousel arrows effect='fade' className='size'>
+          <div>
+            <div className='size' style={{backgroundImage:'url(https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-343369.jpg)'}}/>
+          </div>
+          <div>
+            <div className='size' style={{backgroundImage:'url(https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-4871.jpg)'}}/>
+          </div>
+          <div>
+            <div className='size' style={{backgroundImage:'url(https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-486302.jpg)'}}/>
+          </div>
+          <div>
+            <div className='size' style={{backgroundImage:'url(https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-75202.jpg)'}}/>
+          </div>
+          {/*不用img标签是因为图片大小会出现问题*/}
+          {/*<img style={styles.size} src="https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-343369.jpg" alt="" />*/}
+          {/*<img style={styles.size} src="https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-4871.jpg" alt="" />*/}
+          {/*<img style={styles.size} src="https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-486302.jpg" alt="" />*/}
+          {/*<img style={styles.size} src="https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-75202.jpg" alt="" />*/}
         </Carousel>
       </div>
     )
@@ -22,10 +35,6 @@ const styles = {
     position:'absolute',
     top:0,
     left:0,
-    width:'100%',
-    height:'calc(100vh - 64px)'
-  },
-  size:{
     width:'100%',
     height:'calc(100vh - 64px)'
   }
